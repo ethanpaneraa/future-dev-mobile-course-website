935be5043a6b4113a4df37be4ab6e806fc35d136
Ethan Pineda <joshuapineda66@gmail.com>
2025-01-12 21:11:53 -0600

    figured out routing

---
+++ app/(home)/schedule/page.tsx
@@ -0,0 +1,9 @@
+// import Link from "next/link";
+
+export default function AboutPage() {
+  return (
+    <div className="flex flex-1 flex-col justify-center text-center">
+      <p>this is the schedule page</p>
+    </div>
+  );
+}
